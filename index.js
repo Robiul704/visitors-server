dcdcc6796282e14e19dab96e4bea18c78b099423
robiul704 <robiulislam29935@gmail.com>
2023-11-09 02:24:52 +0600

    this is after deplyment

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const port = process.env.PORT || 5000
 
 
 // midlewer 
-app.use(cors({
-  origin:[`http://localhost:5173`],
-  credentials:true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
